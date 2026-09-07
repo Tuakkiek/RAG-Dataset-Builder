15a9665b7ebcff4f7ec3f887c6f297c7f41ca143
tzaan-sc <tv.ntv.ntvan@gmail.com>
2026-09-07 22:45:21 +0700

    check ndung + bảng: Ndung có thể thiếu sót 1 số ít Bảng có thể lỗi Còn lỗi định dạng

--- test-docling/tzan/test-docling-parse.py
+++ test-docling/tzan/test-docling-parse.py
@@ -4,7 +4,7 @@ converter = DocumentConverter()
 
 # 1. ĐỌC FILE NÀY:
 # Nhớ thêm chữ 'r' ở đầu chuỗi để tránh lỗi đường dẫn trên Windows
-duong_dan_doc = r"D:\GITHUB\RAG-Dataset-Builder\test-docling\data\4.-Artificial-intelligence-and-Privacy-Author-Datatilsynet.pdf"
+duong_dan_doc = r"D:\GITHUB\RAG-Dataset-Builder\test-docling\tzan\(R17A1204) Artificial Intelligence.pdf"
 result = converter.convert(duong_dan_doc) 
 
 # Lấy nội dung văn bản dưới dạng Markdown
@@ -13,7 +13,7 @@ noi_dung = result.document.export_to_markdown()
 # 2. CHỈ ĐỊNH VỊ TRÍ LƯU FILE KẾT QUẢ
 # Điền đường dẫn đầy đủ tới thư mục bạn muốn lưu + tên file (nhớ thêm chữ 'r' ở đầu)
 # Ví dụ mình lưu luôn vào cùng thư mục data:
-duong_dan_luu = r"D:\GITHUB\RAG-Dataset-Builder\test-docling\data\ket_qua.md"
+duong_dan_luu = r"D:\GITHUB\RAG-Dataset-Builder\test-docling\tzan\ket_qua_(R17A1204) Artificial Intelligence.md"
 
 # Mở file tại vị trí đã chỉ định và ghi nội dung vào
 with open(duong_dan_luu, "w", encoding="utf-8") as file:
